fd7f8cc5c9e59e9f20d5491da869748c1a35b2bc
TBERT31 <berteau.thomas@hotmail.fr>
2024-05-13 14:29:53 +0200

    Create Message, User and Rental Repository with JpaRepository

---
+++ backend/src/main/java/com/chatop/backend/repository/RentalRepository.java
@@ -0,0 +1,9 @@
+package com.chatop.backend.repository;
+
+import com.chatop.backend.entity.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface RentalRepository extends JpaRepository<Rental, Integer> {
+
+}
