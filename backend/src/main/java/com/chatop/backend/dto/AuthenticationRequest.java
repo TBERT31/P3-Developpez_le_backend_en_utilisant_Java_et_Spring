5cb1616ac21a527bd5e770fa95652a4684d4f393
TBERT31 <berteau.thomas@hotmail.fr>
2024-05-21 23:08:46 +0200

    control on auth Request attribut

--- backend/src/main/java/com/chatop/backend/dto/AuthenticationRequest.java
+++ backend/src/main/java/com/chatop/backend/dto/AuthenticationRequest.java
@@ -1,6 +1,5 @@
 package com.chatop.backend.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,6 +7,11 @@ import lombok.Data;
 
 @Data
 public class AuthenticationRequest {
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email is not compliant")
     private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 6, max=64, message = "The password must be between 6 and 64 characters long")
     private String password;
 }
